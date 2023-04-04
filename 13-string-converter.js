d70034eb569ee3b6203243e1ca1d98bc5287c8dd
Phaedrus <92445005+ashton8504@users.noreply.github.com>
2023-04-04 09:33:20 -0400

    Update 13-string-converter.js
    adding solution

--- 13-string-converter.js
+++ 13-string-converter.js
@@ -12,8 +12,19 @@ console.log(stringConverter("raccoon")); // => {r: 1, a: 1, c: 2, o: 2, n: 1}
 ***********************************************************************/
 
 function stringConverter(string) {
-  // Your code here
-}
+    // Your code here
+    let letters = string.split("")
+    let result = {}
+
+    letters.forEach(element => {
+        if (result[element] === undefined){
+            result[element] = 1
+        } else {
+            result[element]++
+        }
+    })
+    return result 
+  }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = stringConverter;
